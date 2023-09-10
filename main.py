cb2d4b24950de4d0d8ebd30249ecd2c4150f8404
scotasloth <rossmcc76@gmail.com>
2023-09-10 17:01:05 +0000

    adding draft code

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+import os
+from tkinter import *
+from PIL import ImageTk,Image
+import pyodbc 
+
+def main():
+
+    root = Tk(className="Vaxir Tong Translator") 
+
+    program_directory=sys.path[0]
+
+    database = ()
+
+
+def get_database():
+    pass
+
+
+if __name__ == '__main__':
+    dict = get_database()
+    main(dict)
