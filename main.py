b8e1387c9754827a090fbbb9fb226e7eaf5f6c18
scotasloth <rossmcc76@gmail.com>
2023-09-11 17:33:34 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -4,17 +4,57 @@ from tkinter import *
 from PIL import ImageTk,Image
 import pyodbc 
 
-def main():
+def main(data):
 
+    
     root = Tk(className="Vaxir Tong Translator") 
 
     program_directory=sys.path[0]
 
-    database = ()
+    word = getWord()
+
+    translation = translate(word)
+
 
 
 def get_database():
-    pass
+    dataDict = {}
+    conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\pc\OneDrive\Documents\Programs\Python\Vaxir Tong Translator\Vaxir Tong.accdb;')
+    cursor = conn.cursor()
+    cursor.execute('SELECT English FROM main')
+
+    keys = []
+    val = []
+
+    for row in cursor:
+        new = str(row)
+
+        keys.append(new)
+
+    cursor.execute('SELECT Elder FROM main')
+
+    for row in cursor:
+        new = str(row)
+
+        val.append(new)
+
+    dataDict = {k: v for k, v in zip(keys, val)}
+    #print(str(dataDict))
+   
+    return(dataDict)
+
+
+def getWord():
+
+    word = "('Hello',)"
+    return word
+
+def translate(word):
+
+    elder = dict[word]
+    print (elder)
+    return elder
+
 
 
 if __name__ == '__main__':
